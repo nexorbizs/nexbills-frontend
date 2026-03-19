37a446e725c392b35f502858b46f46d179985df8
rockmuthu <rockmuthu345@gmail.com>
2026-03-19 23:47:18 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainLayout from "./layout/MainLayout";
 
 export default function App() {
 
-  const [mode, setMode] = useState("login");
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    !!localStorage.getItem("currentUser")
-  );
-
-  if (isLoggedIn) return <MainLayout />;
+  const isLoggedIn = !!localStorage.getItem("currentUser");
 
   return (
-    <>
-      {mode === "login" && (
-        <Login
-          setIsLoggedIn={setIsLoggedIn}
-          goSignup={() => setMode("signup")}
-        />
-      )}
+    <BrowserRouter>
 
-      {mode === "signup" && (
-        <Signup
-          goLogin={() => setMode("login")}
+      <Routes>
+
+        {/* LOGIN */}
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login />}
         />
-      )}
-    </>
+
+        {/* SIGNUP */}
+        <Route
+          path="/signup"
+          element={isLoggedIn ? <Navigate to="/dashboard" /> : <Signup />}
+        />
+
+        {/* APP */}
+        <Route
+          path="/*"
+          element={isLoggedIn ? <MainLayout /> : <Navigate to="/" />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
